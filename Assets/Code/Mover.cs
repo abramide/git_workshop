09ae86ca01d6bb4492e28371d0b499988c9b0188
Antti Sinivuori <atsini@gmail.com>
2024-09-20 11:29:56 +0300

    added Mover

---
+++ Assets/Code/Mover.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+{
+    public class Mover : MonoBehaviour
+    {
+        [field: SerializeField]
+        public float Speed
+        {
+            get;
+            private set;
+        }
+        
+        public void Move(Vector2 direction)
+        {
+            transform.position += (Vector3)direction * Speed * Time.deltaTime;
+        }
+    }
+}
