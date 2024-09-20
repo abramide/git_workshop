deb7f29b63a779abeb35525ff3c674ac031067eb
JaniJuhaniKesola <jani.kesola@tuni.fi>
2024-09-20 12:24:01 +0300

    fixed

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-
-{
     public class Mover : MonoBehaviour
     {
         [field: SerializeField]
@@ -15,4 +13,3 @@ using UnityEngine;
             transform.position += (Vector3)direction * Speed * Time.deltaTime;
         }
     }
-}
